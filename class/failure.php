3cae3b90520541e713eae8da3c1e97d7337121ab
vasanth1407 <vasanth@orbiten.in>
2017-11-15 14:26:15 +0530

    updated with failure condition for event registration

---
+++ class/failure.php
@@ -0,0 +1,32 @@
+<?php 
+require_once 'dbconnect.php';
+
+  $status = strip_tags($_POST['status']);
+  $unmappedstatus = strip_tags($_POST['unmappedstatus']);
+  $txnid = strip_tags($_POST['txnid']);
+  $field9 = strip_tags($_POST['field9']);
+  $bank_ref_num = strip_tags($_POST['bank_ref_num']); 
+  
+  $status = $DBcon->real_escape_string($status);
+  $unmappedstatus = $DBcon->real_escape_string($unmappedstatus);
+  $txnid = $DBcon->real_escape_string($txnid);
+  $field9 = $DBcon->real_escape_string($field9);
+  $bank_ref_num = $DBcon->real_escape_string($bank_ref_num);
+
+ 
+  $query = "UPDATE transactions SET status= '$status', unmappedstatus='$unmappedstatus', field9='$field9', bank_ref_num='$bank_ref_num' WHERE txnid = '$txnid'";
+  if($DBcon->query($query)) {
+  		$query = "";
+  		$query = $DBcon->query("SELECT * FROM transactions WHERE txnid = '$txnid'");
+ 		$row = $query->fetch_array(MYSQLI_ASSOC);
+ 		
+ 		$query = "";
+ 		$id = $row['ID'];
+ 		$query = "UPDATE tickets SET Status= '$status', Bibno = NULL, txnid = '$txnid' WHERE transactions_id ='$id'";
+    $DBcon->query($query);
+  }
+
+
+  header("Location: ../events.php");
+
+?>
